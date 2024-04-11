532f5db296f625e5143124fd5faa8c600fb357ca
Okjoon Kim <okjoon.kim@gmail.com>
2024-04-10 22:09:05 -0700

    add an example to analyze the text with the NaturalLanguage framework

---
+++ 16-iOS_ML/04-Natural_Language_Text_Analysis/NaturalLanguageDemo.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: ## Language Recognizor
+import NaturalLanguage
+
+//let message = "אין עשן בלי אש"
+//let message = "Ich wünsche dir einen guten Morgen!"
+//let message = "空穴來風，未必無因"
+var message = "안녕하세요"
+
+if let language = NLLanguageRecognizer.dominantLanguage(for: message) {
+    print("Dtected \(language.rawValue.uppercased()) as dominant lanuage for: \n\"\(message)\"")
+} else {
+    print("Could not recognize language for : \(message)")
+}
+
+let languageRecognizor = NLLanguageRecognizer()
+languageRecognizor.processString(message)
+let languateWithProbabilities = languageRecognizor.languageHypotheses(withMaximum: 3)
+for (language, probability) in languateWithProbabilities {
+    print("Dected \(language.rawValue.uppercased()), probability \(String(format: "%.3f", probability * 100))%)")
+}
+languageRecognizor.reset()
+
+
+//:## String Tokenizer
+//: This playground project shows how to use NLP to dissect a text into semantic units.
+//: Tokenization stands at the core of most other features, so this is an important exercise.
+message = "Knowledge will give you power, but character respect."
+
+let tagger = NLTagger(tagSchemes: [NLTagScheme.tokenType])
+tagger.string = message
+
+tagger.enumerateTags(in: message.startIndex..<message.endIndex,
+                     unit: NLTokenUnit.word,
+                     scheme: NLTagScheme.tokenType,
+                     options: [.omitPunctuation, .omitWhitespace]) { (tag, range) -> Bool in
+    print(message[range])
+    
+    return true
+}
